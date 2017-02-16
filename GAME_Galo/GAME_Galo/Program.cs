7b56fcae2602dd113e9f44d994923ba92fe25c7b
Ricoderp <RicardoCraft23@gmail.com>
2017-02-16 00:25:28 +0000

    Update Program.cs

--- GAME_Galo/GAME_Galo/Program.cs
+++ GAME_Galo/GAME_Galo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,61 @@ namespace GAME_Galo
 {
     class Program
     {
+        static void border()
+        {
+            Console.SetWindowSize(120, 29);
+            Console.Clear();
+            Console.SetCursorPosition(34, 8);
+            Console.WriteLine("#####################################################");
+            Console.SetCursorPosition(34, 16);
+            Console.WriteLine("#####################################################");
+            Console.SetCursorPosition(51, 7);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 6);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 5);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 4);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 3);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 2);
+            Console.WriteLine("#                 #");
+
+            Console.SetCursorPosition(51, 9);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 10);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 11);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 12);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 13);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 14);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 15);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 17);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 18);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 19);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 20);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 21);
+            Console.WriteLine("#                 #");
+            Console.SetCursorPosition(51, 22);
+            Console.WriteLine("#                 #");
+
+            Console.ReadLine();
+            
+        }
+
         static void Main(string[] args)
         {
+            border();
         }
     }
 }
